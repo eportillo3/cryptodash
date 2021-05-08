38151cb26f69c0b688c93c2afcd6cc7f9a45e0c0
Erik Portillo <erikportillo1@gmail.com>
2021-05-08 13:42:54 -0400

    Add App Layout

--- src/App/index.js
+++ src/App/index.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import "./App.css";
 import WelcomeMessage from "./WelcomeMessage";
 import styled, { css } from "styled-components";
+import AppLayout from "./AppLayout";
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <AppLayout>
         <WelcomeMessage />
-      </div>
+      </AppLayout>
     );
   }
 }
