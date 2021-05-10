ea780e7628ef5afd4ac53374e648e3d7855f886e
Erik Portillo <erikportillo1@gmail.com>
2021-05-10 15:16:39 -0400

    Add topSection coinGrid

--- src/Shared/Tile.js
+++ src/Shared/Tile.js
@@ -1,5 +1,10 @@
 import styled from "styled-components";
-import { subtleBoxShadow, lightGreyBackground, greenBoxShadow } from "./Styles";
+import {
+  subtleBoxShadow,
+  lightGreyBackground,
+  greenBoxShadow,
+  redBoxShadow,
+} from "./Styles";
 
 export const Tile = styled.div`
   ${subtleBoxShadow};
@@ -12,4 +17,16 @@ export const SelectableTile = styled(Tile)`
     cursor: pointer;
     ${greenBoxShadow}
   }
+`;
+
+export const DeletableTile = styled(SelectableTile)`
+  &:hover {
+    cursor: pointer;
+    ${redBoxShadow}
+  }
+`;
+
+export const DisabledTile = styled(Tile)`
+  pointer-events: none;
+  opacity: 0.4;
 `;
