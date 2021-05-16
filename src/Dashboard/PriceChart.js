ad7b45986493299a2079e7305845fa5357242537
Erik Portillo <erikportillo1@gmail.com>
2021-05-16 10:04:06 -0400

    Add Highcharts

---
+++ src/Dashboard/PriceChart.js
@@ -0,0 +1,19 @@
+import highchartsConfig from "./HighChartsConfig";
+import React from "react";
+import { Tile } from "../Shared/Tile";
+import { AppContext } from "../App/AppProvider";
+import ReactHighcharts from "react-highcharts";
+import HighchartsTheme from "./HighchartsTheme";
+ReactHighcharts.Highcharts.setOptions(HighchartsTheme);
+
+export default function () {
+  return (
+    <AppContext.Consumer>
+      {({}) => (
+        <Tile>
+          <ReactHighcharts config={highchartsConfig()} />
+        </Tile>
+      )}
+    </AppContext.Consumer>
+  );
+}
